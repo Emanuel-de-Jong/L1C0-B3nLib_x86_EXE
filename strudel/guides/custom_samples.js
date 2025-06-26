dcc7aaf0a324f5eff9c6f63432c42acf46dd5725
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-06-26 17:42:35 +0200

    Samples example

--- strudel/guides/custom_samples.js
+++ strudel/guides/custom_samples.js
@@ -1,3 +1,7 @@
 samples('https://raw.githubusercontent.com/Emanuel-de-Jong/L1C0-B3nLib_x86_EXE/main/assets/audio/strudel.json')
 
-$: n("0 1 2").sound("voice_lines")
+setcpm(120/4)
+
+// $: n("0 1 2").sound("vl_not_enough_minerals")
+
+$: n(irand(3)).struct("x x*2 x x*3").ribbon(20, 2).sound("vl_not_enough_minerals")
